18a7ceb9b20427532d2aa24891bffed3ce745ade
unknown <danghieu1014@gmail.com>
2024-07-18 14:47:29 +0700

    add invalidparameterexception

---
+++ src/main/java/com/project/ecomapp/exception/InvalidParameterException.java
@@ -0,0 +1,8 @@
+package com.project.ecomapp.exception;
+
+public class InvalidParameterException extends Exception{
+
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+}
