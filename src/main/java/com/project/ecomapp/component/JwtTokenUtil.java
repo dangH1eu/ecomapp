fb17066e6248c4e5e65a292985c9bde442f9e500
unknown <danghieu1014@gmail.com>
2024-07-24 17:01:37 +0700

    create jwttokenutil

---
+++ src/main/java/com/project/ecomapp/component/JwtTokenUtil.java
@@ -0,0 +1,75 @@
+package com.project.ecomapp.component;
+
+import com.project.ecomapp.exception.InvalidParameterException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenUtil {
+    @Value("${jwt.expiration}")
+    private long expiration;
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    public String generateToken(com.project.ecomapp.model.User user) throws Exception{
+        Map<String, Object> claims = new HashMap<>();
+//        this.generateSecretKey();
+        claims.put("phoneNumber", user.getPhoneNumber());
+        try {
+            String token = Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getPhoneNumber())
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                    .compact();
+            return token;
+        } catch (Exception e) {
+            throw new InvalidParameterException("cannot create jwt token, error: " + e.getMessage());
+//            return null;
+        }
+    }
+    private String generateSecretKey(){
+        SecureRandom random = new SecureRandom();
+        byte[] keyBytes = new byte[32];
+        random.nextBytes(keyBytes);
+        String secretKey = Encoders.BASE64.encode(keyBytes);
+        return secretKey;
+    }
+
+    private Key getSignInKey() {
+        //Keys.hmacShaKeyFor(Decoders.BASE64.decode("j+Qf9tColF1w33Wp3D9TOj30sfDqDvZYO+b4sn7QiBQ="))
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsTFunction) {
+        final Claims claims = this.extractAllClaims(token);
+        return claimsTFunction.apply(claims);
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expirationDate = this.extractClaim(token, Claims::getExpiration);
+        return expirationDate.before(new Date());
+    }
+
+}
