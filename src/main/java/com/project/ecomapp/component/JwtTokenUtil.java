4550981699ddf5019faa64955e101f6ae405026c
unknown <danghieu1014@gmail.com>
2024-07-28 15:21:06 +0700

    add filter to web security config

--- src/main/java/com/project/ecomapp/component/JwtTokenUtil.java
+++ src/main/java/com/project/ecomapp/component/JwtTokenUtil.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -68,8 +69,16 @@ public class JwtTokenUtil {
     }
 
     public boolean isTokenExpired(String token) {
-        Date expirationDate = this.extractClaim(token, Claims::getExpiration);
+        Date expirationDate = this.extractClaim(token, claims -> claims.getExpiration());
         return expirationDate.before(new Date());
+    }
+    public String extractPhoneNumber(String token){
+        return extractClaim(token, claims -> claims.getSubject());
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        String phoneNumber = extractPhoneNumber(token);
+        return (phoneNumber.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
 }
